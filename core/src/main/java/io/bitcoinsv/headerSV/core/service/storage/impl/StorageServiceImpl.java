be089e2b5b8fecb043c7a269ca20daecfa469210
Ivan J Lozano Fernandez <i.fernandez@nchain.com>
2022-01-25 21:18:55 +0100

    bug fix on the creation of the Genesis block

--- core/src/main/java/io/bitcoinsv/headerSV/core/service/storage/impl/StorageServiceImpl.java
+++ core/src/main/java/io/bitcoinsv/headerSV/core/service/storage/impl/StorageServiceImpl.java
@@ -1,7 +1,12 @@
 package io.bitcoinsv.headerSV.core.service.storage.impl;
 
+import io.bitcoinsv.bitcoinjsv.bitcoin.api.base.HeaderReadOnly;
+import io.bitcoinsv.bitcoinjsv.params.Net;
+import io.bitcoinsv.bitcoinjsv.params.NetworkParameters;
 import io.bitcoinsv.headerSV.core.service.network.impl.NetworkConfiguration;
+import io.bitcoinsv.headerSV.core.service.network.impl.NetworkUtils;
 import io.bitcoinsv.headerSV.core.service.storage.StorageService;
+import io.bitcoinsv.jcl.net.protocol.config.ProtocolConfigBuilder;
 import io.bitcoinsv.jcl.store.blockChainStore.validation.RuleConfigBuilder;
 import io.bitcoinsv.jcl.store.levelDB.blockChainStore.BlockChainStoreLevelDB;
 import io.bitcoinsv.jcl.store.levelDB.blockChainStore.BlockChainStoreLevelDBConfig;
@@ -41,5 +46,39 @@ public class StorageServiceImpl extends BlockChainStoreLevelDB implements Storag
                 true,
                 Duration.ofSeconds(180)
                 );
+    }
+
+    /** Constructor */
+    public StorageServiceImpl(NetworkParameters networkParameters, StorageConfiguration storageConfig) {
+        super (
+                BlockChainStoreLevelDBConfig.chainBuild()
+                        .id("header-sv")
+                        .networkId(ProtocolConfigBuilder.get(networkParameters).getId())
+                        .runtimeConfig(new RuntimeConfigDefault())
+                        .genesisBlock(getGenesisBlock(networkParameters.getNet()))
+                        .orphanPrunningBlockAge(Duration.ofSeconds(storageConfig.getOrphanPruneInterval()))
+                        .forkPrunningHeightDifference(storageConfig.getPruneAfterConfirmations())
+                        .ruleConfig(RuleConfigBuilder.get(networkParameters))
+                        .build(),
+                false,
+                false,
+                null,
+                null,
+                storageConfig.isForkPruningEnabled(),
+                Duration.ofSeconds(180),
+                true,
+                Duration.ofSeconds(180)
+        );
+    }
+
+    // Returns the Genesis block according to the Network
+    private static HeaderReadOnly getGenesisBlock(Net net) {
+        switch (net) {
+            case MAINNET:   return NetworkUtils.GENESIS_BLOCK_HEADER_MAINNET;
+            case TESTNET3:  return NetworkUtils.GENESIS_BLOCK_HEADER_TESTNET;
+            case STN:       return NetworkUtils.GENESIS_BLOCK_HEADER_STNNET;
+            case REGTEST:   return NetworkUtils.GENESIS_BLOCK_HEADER_REGTEST;
+        }
+        throw new RuntimeException("No Genesis Bloc found for the Network " + net);
     }
 }
